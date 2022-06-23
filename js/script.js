57748c4b0bc2c0d4cce66ec6c1275f30a22a4228
StaraaX <100290028+StaraaX@users.noreply.github.com>
2022-06-23 16:04:59 +0100

    uploaded style

--- js/script.js
+++ js/script.js
@@ -7,27 +7,38 @@ function counter() {
     let capL = 0;
     let smallL = 0;
     let space = 0;
-    for (let char of text) {
-        if (char.match(/[aeioyu]/gi) != null) { //vowel
-            vowel++;
-            if (char == char.toUpperCase()) capL++; //vowel and uppercase
-            else smallL++                            // vowel and lowercase
-        }
-
-        else {
-            if (char.match(/[zrtpqsdfghjklmwxcvbn]/gi) != null) { //consonant
-                consonant++;
-                if (char == char.toUpperCase()) capL++;  // consonant and uppercase
-                else smallL++                             // consonant and lowercase
+    if (text === "") alert("Insert some text first ! ")
+    else {
+        for (let char of text) {
+            if (char.match(/[aeioyu]/gi) != null) { //vowel
+                vowel++;
+                if (char == char.toUpperCase()) capL++; //vowel and uppercase
+                else smallL++                            // vowel and lowercase
             }
-        }
-        if (char.match(/[0-9]/gi)) number++;
-        else {
-            if (char.match(/[`!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~]/gi) != null) symbols++;
-            if (char.match(" ") != null) space++;
-        }
 
+            else {
+                if (char.match(/[zrtpqsdfghjklmwxcvbn]/gi) != null) { //consonant
+                    consonant++;
+                    if (char == char.toUpperCase()) capL++;  // consonant and uppercase
+                    else smallL++                             // consonant and lowercase
+                }
+            }
+            if (char.match(/[0-9]/gi)) number++;
+            else {
+                if (char.match(/[`!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~]/gi) != null) symbols++;
+                if (char.match(" ") != null) space++;
+            }
+
+        }
+        alert("Vowels : " + vowel + "\nConsonants :" + consonant + "\n\nNumbers : " + number + "\nSymbols : " + symbols + "\n\nSmall letters :" + smallL + "\nupper letters : " + capL + "\n\nSpaces :" + space);
     }
-    console.log(" Vowels : " + vowel + " Consonants :" + consonant + " numbers : " + number + "  symbols : " + symbols);
-    console.log(" small :" + smallL + " upper : " + capL + " spaces :" + space);
 }
+var input = document.getElementById("textarea");
+input.addEventListener("keypress", (e) => {
+    if (e.key === "Enter") {
+        e.preventDefault();
+        counter();
+    }
+});
+
+
